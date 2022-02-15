5f0e200eda05b1701473cda398b623cd229ea7f3
Dareos Khalili <khalilidareos@gmail.com>
2022-02-15 17:07:24 +0100

    adds model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |i|
+  Pet.create(
+    name: Faker::Creature::Cat.name,
+    address: Faker::Address.full_address,
+    species: %w(dog cat mouse bird).sample
+  )
+
+  puts "pet #{i} is created 🐈"
+end
+
+puts "All pets created successfully!"
